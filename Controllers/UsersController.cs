326c5f1ce577ba1a04b9f327434df471fb60164d
Mauricio Farfan <maufar402@gmail.com>
2020-05-12 14:47:20 -0500

    Adding service for mail

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mailjet.Client;
+using Mailjet.Client.Resources;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MrPiattoWAPI.Model;
+using Newtonsoft.Json.Linq;
 
 namespace MrPiattoWAPI.Controllers
 {
@@ -39,13 +42,84 @@ namespace MrPiattoWAPI.Controllers
         //To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
-        public async Task<int> PostUser(User user)
+        public async Task<int> PostUser(Model.User user)
         {
             user.UnlockedDay = null;
             _context.User.Add(user);
             await _context.SaveChangesAsync();
             var id = _context.User.Where(u => u.Mail == user.Mail).FirstOrDefault();
             return id.Iduser;
+        }
+
+        [HttpGet]
+        public async Task<string> SendMail()
+        {
+            MailjetClient client = new MailjetClient(Environment.GetEnvironmentVariable("36b25d6cba30469cf1cc8911bf79d22a"), 
+                Environment.GetEnvironmentVariable("882892776ad1f8768b813bcc8e8358b0"))
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }.Property(Send.Messages, new JArray 
+                {
+                    new JObject 
+                    {
+                        {
+                            "From",
+                            new JObject 
+                            {
+                                {"Email", "maufar402@gmail.com"},
+                                {"Name", "MAURICIO"}
+                            }
+                        }, 
+                        {
+                            "To",
+                            new JArray 
+                            {
+                                new JObject 
+                                {
+                                    {
+                                      "Email",
+                                      "maufar402@gmail.com"
+                                    }, 
+                                    {
+                                      "Name",
+                                      "MAURICIO"
+                                    }
+                                }           
+                            }
+                        }, 
+                        {
+                           "Subject",
+                           "Greetings from Mailjet."
+                        }, 
+                        {
+                           "TextPart",
+                           "My first Mailjet email"
+                        }, 
+                        {
+                           "HTMLPart",
+                           "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!"
+                        }, 
+                        {
+                           "CustomID",
+                           "AppGettingStartedTest"
+                        }
+                    }
+                });
+            MailjetResponse response = await client.PostAsync(request);
+            if(response.IsSuccessStatusCode)
+            {
+                return "El mensaje se ha enviado correctamente";
+            }
+            else
+            {
+                return $"StatusCode:  {response.StatusCode}\n" +
+                    $"ErrorInfo: {response.GetErrorInfo()}\n" +
+                    $"ErrorMessage: {response.GetErrorMessage()}";
+            }
         }
     }
 }
